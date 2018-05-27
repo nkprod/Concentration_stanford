41bbbbe2f12882107a1f357e870fc7204564e14a
nkprod <nurlybek.karshyga@gmail.com>
2018-05-27 19:55:43 +0600

    Initial Commit

---
+++ Program_one/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Program_one
+//
+//  Created by Nulrybek Karshyga on 27.05.18.
+//  Copyright © 2018 Nulrybek Karshyga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
